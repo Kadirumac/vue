9e2bdadc720b7d8ecd9a42f9cf68b4f50ca00d5f
kadirumac <kadirumac49@gmail.com>
2020-07-22 21:05:40 +0200

    ok

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  // data:{
+  //   checkedNames:[],
+  //   text:'ok',
+  //   options:[
+  //     {value:'Johns'},
+  //     {value:'Paul'},
+  //     {value:'Alex'},
+  //     {value:'Hike'}
+  //   ]
+  // },
+  data(){
+    return{
+      tacos:''
+    }
+  }
+
+})
+
+
