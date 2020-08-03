e4e5ba497cdefd5fe77ce4ce76db5f0e17e50207
kadirumac <kadirumac49@gmail.com>
2020-08-03 22:49:01 +0200

    vue.js

--- src/main.js
+++ src/main.js
@@ -1,24 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-new Vue({
-  el: '#app',
-  // data:{
-  //   checkedNames:[],
-  //   text:'ok',
-  //   options:[
-  //     {value:'Johns'},
-  //     {value:'Paul'},
-  //     {value:'Alex'},
-  //     {value:'Hike'}
-  //   ]
-  // },
-  data(){
-    return{
-      tacos:''
+const App = {
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      name: ''
     }
   }
+}
 
-})
-
-
+Vue.createApp(App).mount('#app')  
