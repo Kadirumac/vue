0f8e8af8c4a7d9c20018215ad40d36964502a4d3
kadirumac <kadirumac49@gmail.com>
2020-10-04 00:11:11 +0200

    first vue practice commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-const App = {
-  data() {
-    return {
-      num1: 0,
-      num2: 0,
-      name: ''
-    }
-  }
-}
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.createApp(App).mount('#app')  
+Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
